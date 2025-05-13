5940cbcae2aeb583c965f4ead6b142acd1cee7da
Girija Mallavarapu <101454131+GIRIJA-M@users.noreply.github.com>
2025-05-13 15:10:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuItems = document.querySelectorAll('nav ul li')
+
+menuItems.forEach((li) =>{
+    li.addEventListener('click', ()=>{
+        removeActiveClass()
+        li.classList.add('active')
+    })
+
+
+})
+
+function removeActiveClass(){
+    menuItems.forEach((li) =>{
+        li.classList.remove('active')
+    })
+}
